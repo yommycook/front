a25f910d08dda59ac505b4c7948b07414508fe1a
Jin <2414487@gmail.com>
2023-05-09 06:45:22 +0900

    메인화면 레이아웃

---
+++ pages/_app.js
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import 'antd/dist/reset.css';
+import React, { useState } from 'react';
+import { Typography, Input, Avatar, Button } from 'antd';
+
+const { Title } = Typography;
+
+const App = ({ Component }) => {
+    return (
+        <>
+            <Head>
+                <meta charSet='utf-8' />
+                <title>요미</title>
+            </Head>
+            <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                <Title>요미</Title>
+            </div>
+            <div
+                style={{
+                    display: 'inline-block',
+                    verticalAlign: 'middle',
+                    marginLeft: '30px',
+                    marginRight: '30px',
+                }}
+            >
+                <Input.Search />
+            </div>
+            <div style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+                <Avatar
+                    style={{ backgroundColor: 'slateblue', verticalAlign: 'middle' }}
+                    size='large'
+                >
+                    지니
+                </Avatar>
+                <Button
+                    size='small'
+                    style={{ margin: '0 16px', verticalAlign: 'middle' }}
+                >
+                    로그아웃
+                </Button>
+            </div>
+            <Component />
+        </>
+    );
+};
+
+App.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+};
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,48 @@
+import React from 'react'; //안 써도 상관 없음! (Next에서는 걍 실행 가능! pages라는 디렉토리를 알아본당)
+import AppLayout from '../components/AppLayout';
+import Head from 'next/head';
+import Add from '../components/add';
+import CardBoard from '../components/cardboard';
+
+const Home = () => {
+    return (
+        <>
+            <Head>
+                <meta charSet='utf-8' />
+                <title>요미|나의 레시피</title>
+            </Head>
+            <AppLayout>
+                <Add />
+                <div
+                    style={{
+                        display: 'inline-block',
+                        verticalAlign: 'middle',
+                        margin: '3ㄴ0px',
+                    }}
+                >
+                    <CardBoard />
+                </div>
+                <div
+                    style={{
+                        display: 'inline-block',
+                        verticalAlign: 'middle',
+                        margin: '30px',
+                    }}
+                >
+                    <CardBoard />
+                </div>
+                <div
+                    style={{
+                        display: 'inline-block',
+                        verticalAlign: 'middle',
+                        margin: '30px',
+                    }}
+                >
+                    <CardBoard />
+                </div>
+            </AppLayout>
+        </>
+    );
+};
+
+export default Home;
